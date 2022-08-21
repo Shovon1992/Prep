c943cf9072b8c90e615fdfd0e828ec442bedf363
Shovon WP <shov.mondal92@gmail.com>
2022-08-22 02:38:48 +0530

    Bubble sort

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("This is for the Test js files in locally ")
+import BubbleSort from "./Algorithm/Sorting/Bubblesort.js"
+const testArray = [
+    [7,3,2,1],
+    [12,3,12,12,1,-166,1,2,-546,19,3,-7,1,779,-11],
+    [12,3,12,12,1,66,1,2,546,19,3,7,1,779,11]
+    
+] 
+
+for(let i=0 ; i<testArray.length; i++){
+    console.log(`\n------------------------------------------------Test ${i+1}------------------------------------------------\n`)
+    BubbleSort(testArray[i])
+    console.log(`\n`)
+
+}
